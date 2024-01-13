896a87e1920fbe26500f4474002352a3f607e330
brcshakil1 <brcshakil72@gmail.com>
2024-01-13 15:41:45 +0600

    desktop navbar

--- eBuy-client/src/components/shared/Navbar.jsx
+++ eBuy-client/src/components/shared/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import SearchProducts from "./SearchProducts";
 import PersonIcon from "@mui/icons-material/Person";
 import { Container } from "@mui/material";
@@ -29,14 +29,42 @@ const Navbar = () => {
   return (
     <div className="bg-white md:bg-primary py-2.5 md:py-4 fixed bottom-0 w-full md:sticky md:top-0  ">
       <Container>
-        <div className="flex justify-between items-center">
+        <div className="flex justify-between items-center ">
           <Link
             to="/"
-            className="text-neutral text-4xl font-bold hidden md:block"
+            className="text-neutral text-4xl font-bold hidden md:block "
           >
             eBuy
           </Link>
-          <SearchProducts />
+          <div>
+            <SearchProducts />
+          </div>
+          {/* Desktop nav links */}
+          <ul className="hidden md:flex items-center gap-6 ">
+            <NavLink to="/login">
+              <li className="hover:bg-[#38383891] text-white px-3 py-2 rounded-xl">
+                <ShoppingCartIcon></ShoppingCartIcon>
+              </li>
+            </NavLink>
+
+            <NavLink to="/login" className="">
+              <li className="hover:bg-[#38383891] text-white px-3 py-2 rounded-xl font-semibold">
+                Login
+              </li>
+            </NavLink>
+            <div className="w-[1px] h-[15px] bg-white"></div>
+            <NavLink to="/login">
+              <li className="hover:bg-[#38383891] text-white px-3 py-2 rounded-xl font-semibold">
+                Sign Up
+              </li>
+            </NavLink>
+
+            <NavLink to="/account">
+              <li className="hover:bg-[#38383891] text-white px-3 py-2 rounded-xl">
+                <PersonIcon />
+              </li>
+            </NavLink>
+          </ul>
           {/* Mobile nav links */}
           <ul className="flex justify-between items-center w-full md:hidden">
             {mobileNavLinks}
--- eBuy-client/src/components/shared/SearchProducts.jsx
+++ eBuy-client/src/components/shared/SearchProducts.jsx
@@ -2,10 +2,14 @@ import SearchIcon from "@mui/icons-material/Search";
 
 const SearchProducts = () => {
   return (
-    <form className="border-2 border-gray-500">
-      <input type="text" placeholder="Search in eBuy" className="border" />
-      <button>
-        <SearchIcon />
+    <form className="absolute md:relative top-0 lg:w-[550px] border-2 hover:border-slate-400 md:w-[390px] bg-neutral flex justify-between rounded-full overflow-hidden">
+      <input
+        type="text"
+        placeholder="Search in eBuy"
+        className="border-none lg:w-[500px] px-3 py-2 outline-none"
+      />
+      <button className="lg:w-[50px] group border-none ">
+        <SearchIcon className="text-xl mx-auto text-slate-400 group-hover:text-primary " />
       </button>
     </form>
   );
